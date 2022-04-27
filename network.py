d99793f3728f0baa1362a7f126035be6e058adb3
thebrard <thomas.hebrard134@gmail.com>
2022-04-27 16:33:24 +0200

    black

--- network.py
+++ network.py
@@ -1,34 +1,10 @@
-# importing the libraries
-import pandas as pd
-import numpy as np
-
-# for reading and displaying images
-from skimage.io import imread
-from skimage.io import imshow
-import matplotlib.pyplot as plt
-
-# for creating validation set
-from sklearn.model_selection import train_test_split
-
-# for evaluating the model
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
-
-# PyTorch libraries and modules
-import torch
-from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout, ConvTranspose2d
-from torch.optim import Adam, SGD
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.autograd import Variable
-from collections import OrderedDict
 from torch.nn import init
 
-def conv3x3(in_channels, out_channels, stride=1, 
-            padding=1, bias=True, groups=1):    
+
+def conv3x3(in_channels, out_channels, stride=1, padding=1, bias=True, groups=1):
     return nn.Conv2d(
         in_channels,
         out_channels,
@@ -36,29 +12,24 @@ def conv3x3(in_channels, out_channels, stride=1,
         stride=stride,
         padding=padding,
         bias=bias,
-        groups=groups)
+        groups=groups,
+    )
 
-def upconv2x2(in_channels, out_channels, mode='transpose'):
-    if mode == 'transpose':
-        return nn.ConvTranspose2d(
-            in_channels,
-            out_channels,
-            kernel_size=2,
-            stride=2)
+
+def upconv2x2(in_channels, out_channels, mode="transpose"):
+    if mode == "transpose":
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
     else:
         # out_channels is always going to be the same
         # as in_channels
         return nn.Sequential(
-            nn.Upsample(mode='bilinear', scale_factor=2),
-            conv1x1(in_channels, out_channels))
+            nn.Upsample(mode="bilinear", scale_factor=2),
+            conv1x1(in_channels, out_channels),
+        )
+
 
 def conv1x1(in_channels, out_channels, groups=1):
-    return nn.Conv2d(
-        in_channels,
-        out_channels,
-        kernel_size=1,
-        groups=groups,
-        stride=1)
+    return nn.Conv2d(in_channels, out_channels, kernel_size=1, groups=groups, stride=1)
 
 
 class DownConv(nn.Module):
@@ -66,6 +37,7 @@ class DownConv(nn.Module):
     A helper Module that performs 2 convolutions and 1 MaxPool.
     A ReLU activation follows each convolution.
     """
+
     def __init__(self, in_channels, out_channels, pooling=True):
         super(DownConv, self).__init__()
 
@@ -93,8 +65,10 @@ class UpConv(nn.Module):
     A helper Module that performs 2 convolutions and 1 UpConvolution.
     A ReLU activation follows each convolution.
     """
-    def __init__(self, in_channels, out_channels, 
-                 merge_mode='concat', up_mode='transpose'):
+
+    def __init__(
+        self, in_channels, out_channels, merge_mode="concat", up_mode="transpose"
+    ):
         super(UpConv, self).__init__()
 
         self.in_channels = in_channels
@@ -102,26 +76,23 @@ class UpConv(nn.Module):
         self.merge_mode = merge_mode
         self.up_mode = up_mode
 
-        self.upconv = upconv2x2(self.in_channels, self.out_channels, 
-            mode=self.up_mode)
+        self.upconv = upconv2x2(self.in_channels, self.out_channels, mode=self.up_mode)
 
-        if self.merge_mode == 'concat':
-            self.conv1 = conv3x3(
-                2*self.out_channels, self.out_channels)
+        if self.merge_mode == "concat":
+            self.conv1 = conv3x3(2 * self.out_channels, self.out_channels)
         else:
             # num of input channels to conv2 is same
             self.conv1 = conv3x3(self.out_channels, self.out_channels)
         self.conv2 = conv3x3(self.out_channels, self.out_channels)
 
-
     def forward(self, from_down, from_up):
-        """ Forward pass
+        """Forward pass
         Arguments:
             from_down: tensor from the encoder pathway
             from_up: upconv'd tensor from the decoder pathway
         """
         from_up = self.upconv(from_up)
-        if self.merge_mode == 'concat':
+        if self.merge_mode == "concat":
             x = torch.cat((from_up, from_down), 1)
         else:
             x = from_up + from_down
@@ -129,8 +100,9 @@ class UpConv(nn.Module):
         x = F.relu(self.conv2(x))
         return x
 
+
 class UNet(nn.Module):
-    """ `UNet` class is based on https://arxiv.org/abs/1505.04597
+    """`UNet` class is based on https://arxiv.org/abs/1505.04597
     The U-Net is a convolutional encoder-decoder neural network.
     Contextual spatial information (from the decoding,
     expansive pathway) about an input tensor is merged with
@@ -150,15 +122,21 @@ class UNet(nn.Module):
         the tranpose convolution (specified by upmode='transpose')
     """
 
-    def __init__(self, num_classes, in_channels=1, depth=5, 
-                 start_filts=64, up_mode='transpose', 
-                 merge_mode='concat'):
+    def __init__(
+        self,
+        num_classes,
+        in_channels=1,
+        depth=5,
+        start_filts=64,
+        up_mode="transpose",
+        merge_mode="concat",
+    ):
         """
         Arguments:
             in_channels: int, number of channels in the input tensor.
                 Default is 3 for RGB images.
             depth: int, number of MaxPools in the U-Net.
-            start_filts: int, number of convolutional filters for the 
+            start_filts: int, number of convolutional filters for the
                 first conv.
             up_mode: string, type of upconvolution. Choices: 'transpose'
                 for transpose convolution or 'upsample' for nearest neighbour
@@ -166,28 +144,34 @@ class UNet(nn.Module):
         """
         super(UNet, self).__init__()
 
-        if up_mode in ('transpose', 'upsample'):
+        if up_mode in ("transpose", "upsample"):
             self.up_mode = up_mode
         else:
-            raise ValueError("\"{}\" is not a valid mode for "
-                             "upsampling. Only \"transpose\" and "
-                             "\"upsample\" are allowed.".format(up_mode))
-    
-        if merge_mode in ('concat', 'add'):
+            raise ValueError(
+                '"{}" is not a valid mode for '
+                'upsampling. Only "transpose" and '
+                '"upsample" are allowed.'.format(up_mode)
+            )
+
+        if merge_mode in ("concat", "add"):
             self.merge_mode = merge_mode
         else:
-            raise ValueError("\"{}\" is not a valid mode for"
-                             "merging up and down paths. "
-                             "Only \"concat\" and "
-                             "\"add\" are allowed.".format(up_mode))
+            raise ValueError(
+                '"{}" is not a valid mode for'
+                "merging up and down paths. "
+                'Only "concat" and '
+                '"add" are allowed.'.format(up_mode)
+            )
 
         # NOTE: up_mode 'upsample' is incompatible with merge_mode 'add'
-        if self.up_mode == 'upsample' and self.merge_mode == 'add':
-            raise ValueError("up_mode \"upsample\" is incompatible "
-                             "with merge_mode \"add\" at the moment "
-                             "because it doesn't make sense to use "
-                             "nearest neighbour to reduce "
-                             "depth channels (by half).")
+        if self.up_mode == "upsample" and self.merge_mode == "add":
+            raise ValueError(
+                'up_mode "upsample" is incompatible '
+                'with merge_mode "add" at the moment '
+                "because it doesn't make sense to use "
+                "nearest neighbour to reduce "
+                "depth channels (by half)."
+            )
 
         self.num_classes = num_classes
         self.in_channels = in_channels
@@ -200,19 +184,18 @@ class UNet(nn.Module):
         # create the encoder pathway and add to a list
         for i in range(depth):
             ins = self.in_channels if i == 0 else outs
-            outs = self.start_filts*(2**i)
-            pooling = True if i < depth-1 else False
+            outs = self.start_filts * (2**i)
+            pooling = True if i < depth - 1 else False
 
             down_conv = DownConv(ins, outs, pooling=pooling)
             self.down_convs.append(down_conv)
 
         # create the decoder pathway and add to a list
         # - careful! decoding only requires depth-1 blocks
-        for i in range(depth-1):
+        for i in range(depth - 1):
             ins = outs
             outs = ins // 2
-            up_conv = UpConv(ins, outs, up_mode=up_mode,
-                merge_mode=merge_mode)
+            up_conv = UpConv(ins, outs, up_mode=up_mode, merge_mode=merge_mode)
             self.up_convs.append(up_conv)
 
         self.conv_final = conv1x1(outs, self.num_classes)
@@ -229,11 +212,9 @@ class UNet(nn.Module):
             init.xavier_normal(m.weight)
             init.constant(m.bias, 0)
 
-
     def reset_params(self):
         for i, m in enumerate(self.modules()):
             self.weight_init(m)
-
 
     def forward(self, x):
         encoder_outs = []
@@ -242,9 +223,9 @@ class UNet(nn.Module):
             x, before_pool = module(x)
             encoder_outs.append(before_pool)
         for i, module in enumerate(self.up_convs):
-            before_pool = encoder_outs[-(i+2)]
+            before_pool = encoder_outs[-(i + 2)]
             x = module(before_pool, x)
-        
+
         # No softmax is used. This means you need to use
         # nn.CrossEntropyLoss is your training script,
         # as this module includes a softmax already.
--- train.py
+++ train.py
@@ -1,68 +1,45 @@
-
-
 from get_datas import get_datas
 from network import UNet
 from PIL import Image
+
 # importing the libraries
-import pandas as pd
 import numpy as np
-
-# for reading and displaying images
-from skimage.io import imread
-from skimage.io import imshow
-import matplotlib.pyplot as plt
-
-# for creating validation set
-from sklearn.model_selection import train_test_split
-
-# for evaluating the model
-from sklearn.metrics import accuracy_score
-from tqdm import tqdm
 
 # PyTorch libraries and modules
 import torch
-from torch.autograd import Variable
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, Dropout, ConvTranspose2d
-from torch.optim import Adam, SGD
-
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-from collections import OrderedDict
-from torch.nn import init
 
 
 train_x, label_x = get_datas(10)
-#train_x1 = train_x_all[0:15]
-#label_x1 = label_x_all[0:15]
+# train_x1 = train_x_all[0:15]
+# label_x1 = label_x_all[0:15]
 m = nn.Softmax(dim=1)
 loss = nn.CrossEntropyLoss()
 
 network = UNet(10)
 
-for p in range(0,10):
-    print('Round {}'.format(p))
-    #indices = torch.randperm(len(train_x1))[:10]
-    #train_x = train_x1[indices]
-    #label_x = label_x1[indices]
+for p in range(0, 10):
+    print("Round {}".format(p))
+    # indices = torch.randperm(len(train_x1))[:10]
+    # train_x = train_x1[indices]
+    # label_x = label_x1[indices]
 
     result = network(train_x)
-    result_soft =m(result)
+    result_soft = m(result)
 
     output = loss(result_soft, label_x.long())
     output.backward()
 
     image_result2 = result_soft[1]
     image_result2.shape
-    one_label_result = torch.empty((512,512), dtype=torch.int64)
-    for i in range(0,512):
-        for j in range(0,512):
-            if image_result2[0,i,j]>image_result2[1,i,j]:
-                one_label_result[i,j] = 255
-            if image_result2[0,i,j]<=image_result2[1,i,j]:
-                one_label_result[i,j] = 0
-    one_label_result =np.array(one_label_result).astype(np.uint8)
+    one_label_result = torch.empty((512, 512), dtype=torch.int64)
+    for i in range(0, 512):
+        for j in range(0, 512):
+            if image_result2[0, i, j] > image_result2[1, i, j]:
+                one_label_result[i, j] = 255
+            if image_result2[0, i, j] <= image_result2[1, i, j]:
+                one_label_result[i, j] = 0
+    one_label_result = np.array(one_label_result).astype(np.uint8)
     im = Image.fromarray(one_label_result)
     im.save("results/result_{}.png".format(p))
 torch.save(network, "results")
