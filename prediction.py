ff5969243d53c27d205f6e28599aeeb4ff68853a
Loïc <loic.kervellec@polytechnique.edu>
2022-05-03 00:25:16 +0200

    predictions

---
+++ prediction.py
@@ -0,0 +1,73 @@
+import os
+import torch
+from first_visu import BuildingsDataset, get_preprocessing, get_training_augmentation, get_validation_augmentation, colour_code_segmentation, reverse_one_hot, visualize
+import numpy as np
+import cv2
+
+# Set device: `cuda` or `cpu`
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+if os.path.exists('./best_model.pth'):
+    best_model = torch.load('./best_model.pth', map_location=DEVICE)
+    print('Loaded UNet model from this run.')
+
+class_rgb_values = [[0, 0, 0], [255, 255, 255]]
+select_class_rgb_values =  np.array(class_rgb_values)
+
+x_train_dir = os.path.join(DATA_DIR, 'train_images')
+y_train_dir = os.path.join(DATA_DIR, 'train_labels')
+
+sample_preds_folder = 'sample_predictions/'
+if not os.path.exists(sample_preds_folder):
+    os.makedirs(sample_preds_folder)
+
+train_dataset = BuildingsDataset(
+    x_train_dir, y_train_dir, 
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocessing_fn=None),
+    class_rgb_values=select_class_rgb_values,
+)
+
+train_dataset_vis = BuildingsDataset(
+    x_train_dir, y_train_dir, 
+    preprocessing=get_preprocessing(preprocessing_fn=None),
+    class_rgb_values=select_class_rgb_values,
+)
+
+
+def crop_image(image, target_image_dims=[1500,1500,3]):
+   
+    target_size = target_image_dims[0]
+    image_size = len(image)
+    padding = (image_size - target_size) // 2
+
+    return image[
+        padding:image_size - padding,
+        padding:image_size - padding,
+        :,
+    ]
+
+for idx in range(len(train_dataset)):
+
+    image, gt_mask = train_dataset[idx]
+    image_vis = crop_image(train_dataset_vis[idx][0].astype('uint8'))
+    x_tensor = torch.from_numpy(image).to(DEVICE).unsqueeze(0)
+    # Predict test image
+    pred_mask = best_model(x_tensor)
+    pred_mask = pred_mask.detach().squeeze().cpu().numpy()
+    # Convert pred_mask from `CHW` format to `HWC` format
+    pred_mask = np.transpose(pred_mask,(1,2,0))
+    # Get prediction channel corresponding to building
+    pred_building_heatmap = pred_mask[:,:,1]
+    pred_mask = crop_image(colour_code_segmentation(reverse_one_hot(pred_mask), select_class_rgb_values))
+    # Convert gt_mask from `CHW` format to `HWC` format
+    gt_mask = np.transpose(gt_mask,(1,2,0))
+    gt_mask = crop_image(colour_code_segmentation(reverse_one_hot(gt_mask), select_class_rgb_values))
+    cv2.imwrite(os.path.join(sample_preds_folder, f"sample_pred_{idx}.png"), np.hstack([image_vis, gt_mask, pred_mask])[:,:,::-1])
+    
+    visualize(
+        original_image = image_vis,
+        ground_truth_mask = gt_mask,
+        predicted_mask = pred_mask,
+        predicted_building_heatmap = pred_building_heatmap
+    )
